17e135cd490b7f8eb8a5c5807e03db4b63873a6f
Eetu Sjöblom <eetu.sjoblom@silo.ai>
2021-06-07 22:23:27 +0300

    fix

--- train.py
+++ train.py
@@ -1,184 +1,26 @@
 import argparse
 import logging
-import os
-from collections import deque
 from functools import partial
 
 import datasets
-import numpy as np
-import torch
+import trainer
 import transformers
 from accelerate import Accelerator
 from datasets import load_dataset, load_metric
 from torch.utils.data.dataloader import DataLoader
-from tqdm import tqdm
 from transformers import (
     AdamW,
     AutoModelForSequenceClassification,
     AutoTokenizer,
     DataCollatorWithPadding,
-    SchedulerType,
     get_scheduler,
     set_seed,
 )
 
-
-class Trainer:
-    def __init__(
-        self,
-        model,
-        model_dir,
-        tokenizer,
-        optimizer,
-        accelerator,
-        dataloader_train,
-        dataloader_eval,
-        batch_size_train,
-        batch_size_eval,
-        max_steps,
-        eval_steps,
-        save_steps,
-        log_steps,
-        metric_name,
-    ):
-        self.model = model
-        self.model_dir = model_dir
-        self.tokenizer = tokenizer
-        self.optimizer = optimizer
-        self.accelerator = accelerator
-
-        self.dataloader_train = dataloader_train
-        self.dataloader_eval = dataloader_eval
-        self.batch_size_train = batch_size_train
-        self.batch_size_eval = batch_size_eval
-        self.gradient_accumulation_steps = 1
-        self.max_steps = max_steps
-        self.eval_steps = eval_steps
-        self.save_steps = save_steps
-        self.log_steps = log_steps
-
-        self.best_model_checkpoint = None
-        self.best_model_score = None
-
-        self.metric_name = metric_name
-        self.metric = load_metric(self.metric_name)
-
-    def compute_metrics(self, predictions, labels):
-        if self.metric_name in ["accuracy", "f1"]:
-            predictions = (predictions > 0.5).astype(int)
-
-        if self.metric_name == "accuracy":
-            result = self.metric.compute(predictions=predictions, references=labels)
-        elif self.metric_name == "f1":
-            result = self.metric.compute(
-                predictions=predictions, references=labels, average="macro"
-            )
-
-        return result
-
-    def evaluate(self, dataloader):
-        self.model.eval()
-        predictions, labels = [], []
-        for step, batch in enumerate(self.dataloader_eval):
-            outputs = self.model(**batch)
-            predictions.extend(
-                torch.sigmoid(self.accelerator.gather(outputs.logits))
-                .detach()
-                .view(-1)
-                .tolist()
-            )
-            labels.extend(batch["labels"].tolist())
-        self.model.train()
-        eval_metric = self.compute_metrics(np.array(predictions), labels)
-        return eval_metric
-
-    def train(self):
-        total_batch_size = self.batch_size_train * self.accelerator.num_processes
-
-        num_samples = len(self.dataloader_train.dataset)
-        logging.info("***** Running training *****")
-        logging.info(f"  Num samples = {num_samples}")
-        logging.info(f"  Batch size per device = {self.batch_size_train}")
-        logging.info(
-            f"  Total train batch size (w. parallel, distributed & accumulation) "
-            f"= {total_batch_size}"
-        )
-        logging.info(
-            f"  Gradient Accumulation steps = {self.gradient_accumulation_steps}"
-        )
-        logging.info(f"  Total optimization steps = {self.max_steps}")
-        logging.info(
-            f"  Total epochs approx. = "
-            f"{(self.max_steps*total_batch_size)/num_samples:.2f}"
-        )
-        progress_bar = tqdm(
-            range(self.max_steps), disable=not self.accelerator.is_local_main_process
-        )
-        losses = deque(maxlen=args.log_steps // 2)
-        continue_training, completed_steps = True, 0
-        while continue_training:
-            self.model.train()
-            for step, batch in enumerate(self.dataloader_train):
-                outputs = self.model(**batch)
-                loss = outputs.loss
-                self.accelerator.backward(loss)
-                self.optimizer.step()
-                self.optimizer.zero_grad()
-                progress_bar.update(1)
-                completed_steps += 1
-
-                losses.append(loss.item())
-
-                if completed_steps >= self.max_steps:
-                    continue_training = False
-                    break
-
-                if completed_steps % self.log_steps == 0:
-                    logging.info(
-                        f"Step {completed_steps}: train loss {sum(losses)/len(losses):.5f}"
-                    )
-
-                eval_metric = None
-                if (completed_steps > 0) and (completed_steps % self.eval_steps == 0):
-                    logging.info("Evaluating.")
-                    eval_metric = self.evaluate(self.dataloader_eval)
-                    logging.info(f"Step {completed_steps}: {eval_metric}")
-
-                if completed_steps % self.save_steps == 0:
-                    self.accelerator.wait_for_everyone()
-                    unwrapped_model = self.accelerator.unwrap_model(self.model)
-                    chkpt_dir = os.path.join(
-                        self.model_dir, f"checkpoint-{completed_steps}"
-                    )
-                    unwrapped_model.save_pretrained(
-                        chkpt_dir, save_function=self.accelerator.save
-                    )
-                    self.accelerator.save(
-                        self.optimizer, os.path.join(chkpt_dir, "optimizer.pt")
-                    )
-                    self.accelerator.save(
-                        self.tokenizer, os.path.join(chkpt_dir, "tokenizer.pt")
-                    )
-                    # If we did not evaluate at this step, we must get the
-                    # evaluate to get the metric for comparison to earlier checkpoints
-                    if eval_metric is None:
-                        eval_metric = self.evaluate(self.dataloader_eval)
-
-                    if (self.best_model_score is None) or (
-                        eval_metric[self.metric_name] > self.best_model_score
-                    ):
-                        self.best_model_score = eval_metric[self.metric_name]
-                        self.best_model_checkpoint = chkpt_dir
-
-                    # FIXME: USE MLFLOW ARTIFACT LOGGING
-                    # if (self.mlflow_tracking_uri is not None) and (
-                    #     self.mlflow_save_models == "all"
-                    # ):
-                    #     # FIXME: Save model to mlflow artifact store
-                    #     pass
+logger = logging.getLogger(__name__)
 
 
-def tokenize_data(examples, tokenizer):
+def tokenize_data(examples, tokenizer, label2id):
     res = tokenizer(
         examples["sentence1"],
         examples["sentence2"],
@@ -186,7 +28,10 @@ def tokenize_data(examples, tokenizer):
         max_length=tokenizer.model_max_length,
         truncation=True,
     )
-    res["labels"] = examples["label"]
+    if type(examples["label"][0]) == int:
+        res["labels"] = examples["label"]
+    else:
+        res["labels"] = [label2id[label] for label in examples["label"]]
     return res
 
 
@@ -216,6 +61,7 @@ def prepare_optimizer(optimizer, model, weight_decay, lr):
 
 
 def main(args):
+    set_seed(args.seed)
     accelerator = Accelerator()
     # Make one log on every process with the configuration for debugging.
     logging.basicConfig(
@@ -223,7 +69,7 @@ def main(args):
         datefmt="%m/%d/%Y %H:%M:%S",
         level=logging.INFO,
     )
-    logging.info(accelerator.state)
+    logger.info(accelerator.state)
 
     if accelerator.is_local_main_process:
         datasets.utils.logging.set_verbosity_warning()
@@ -232,22 +78,33 @@ def main(args):
         datasets.utils.logging.set_verbosity_error()
         transformers.utils.logging.set_verbosity_error()
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model, cache_dir=args.cache)
-    model = AutoModelForSequenceClassification.from_pretrained(
-        args.model, num_labels=1, cache_dir=args.cache
-    )
-
     dataset = load_dataset(
         "json",
         cache_dir=args.cache,
         data_files={"train": args.train_data, "validation": args.dev_data},
+    )
+    labels = dataset["train"].unique("label")
+    label2id = None
+    if type(labels[0]) == str:
+        label2id = {label: i for i, label in enumerate(sorted(labels))}
+    elif type(labels[0]) == int:
+        label2id = {f"label_{i}": i for i in sorted(labels)}
+    num_labels = len(labels)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model, cache_dir=args.cache)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model,
+        num_labels=num_labels,
+        label2id=label2id,
+        id2label={v: k for k, v in label2id.items()},
+        cache_dir=args.cache,
     )
 
     cols_to_remove = [
         k for k in list(dataset["train"][0].keys()) if k not in ["labels"]
     ]
     dataset = dataset.map(
-        partial(tokenize_data, tokenizer=tokenizer),
+        partial(tokenize_data, tokenizer=tokenizer, label2id=label2id),
         batched=True,
         remove_columns=cols_to_remove,
     )
@@ -274,11 +131,34 @@ def main(args):
         model, optimizer, train_dataloader, eval_dataloader
     )
 
-    trainer = Trainer(
+    lr_scheduler = get_scheduler(
+        name=args.lr_scheduler,
+        optimizer=optimizer,
+        num_warmup_steps=args.warmup_steps,
+        num_training_steps=args.max_steps,
+    )
+
+    metric_f1 = load_metric("f1")
+    metric_accuracy = load_metric("accuracy")
+
+    def compute_metrics(predictions, labels):
+        predictions = (predictions > 0.5).astype(int)
+
+        result = metric_accuracy.compute(predictions=predictions, references=labels)
+
+        for typ in ["macro", "micro", "binary"]:
+            result[f"f1_{typ}"] = metric_f1.compute(
+                predictions=predictions, references=labels, average=typ
+            ).pop("f1")
+
+        return result
+
+    trainer_obj = trainer.Trainer(
         model=model,
         tokenizer=tokenizer,
         model_dir=args.model_dir,
         optimizer=optimizer,
+        scheduler=lr_scheduler,
         accelerator=accelerator,
         dataloader_train=train_dataloader,
         dataloader_eval=eval_dataloader,
@@ -289,13 +169,18 @@ def main(args):
         save_steps=args.save_steps,
         log_steps=args.log_steps,
         metric_name=args.metric_name,
+        metric_fn=compute_metrics,
     )
 
-    trainer.train()
+    trainer_obj.train()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fine-tune BERT.")
+    parser.add_argument(
+        "--seed", default=42, required=False, help="Random seed for reproducibility"
+    )
+
     md_args = parser.add_argument_group("Model and data")
     md_args.add_argument("--model", default="bert-base-cased")
     md_args.add_argument(
@@ -348,8 +233,6 @@ if __name__ == "__main__":
         default=0.01,
         help="Weight decay regularization parameter",
     )
-    # train_args.add_argument("--log_freq", type=int, default=250,
-    #                         help="log training every N steps")
     train_args.add_argument(
         "--eval_steps", type=int, default=500, help="Evaluate on dev every n steps"
     )
@@ -363,8 +246,28 @@ if __name__ == "__main__":
         "--metric_name",
         type=str,
         required=False,
-        default="f1",
+        default="f1_micro",
         help="Metric name for evaluation",
+        choices=["f1_samples", "f1_micro", "f1_macro", "f1_binary", "accuracy"],
+    )
+    train_args.add_argument(
+        "--lr_scheduler",
+        default="linear",
+        help="The scheduler type to use.",
+        choices=[
+            "linear",
+            "cosine",
+            "cosine_with_restarts",
+            "polynomial",
+            "constant",
+            "constant_with_warmup",
+        ],
+    )
+    train_args.add_argument(
+        "--warmup_steps",
+        type=int,
+        default=0,
+        help="Number of warmup steps in the lr scheduler.",
     )
     args = parser.parse_args()
 
---
+++ trainer.py
@@ -0,0 +1,163 @@
+import logging
+import os
+from collections import deque
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+
+logger = logging.getLogger(__name__)
+
+
+class Trainer:
+    def __init__(
+        self,
+        model,
+        model_dir,
+        tokenizer,
+        optimizer,
+        scheduler,
+        accelerator,
+        dataloader_train,
+        dataloader_eval,
+        batch_size_train,
+        batch_size_eval,
+        max_steps,
+        eval_steps,
+        save_steps,
+        log_steps,
+        metric_name,
+        metric_fn,
+    ):
+        self.model = model
+        self.model_dir = model_dir
+        self.tokenizer = tokenizer
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.accelerator = accelerator
+
+        self.dataloader_train = dataloader_train
+        self.dataloader_eval = dataloader_eval
+        self.batch_size_train = batch_size_train
+        self.batch_size_eval = batch_size_eval
+        self.gradient_accumulation_steps = 1
+        self.max_steps = max_steps
+        self.eval_steps = eval_steps
+        self.save_steps = save_steps
+        self.log_steps = log_steps
+
+        self.best_model_checkpoint = None
+        self.best_model_score = None
+
+        self.metric_name = metric_name
+        self.metric_fn = metric_fn
+
+    def evaluate(self, dataloader):
+        self.model.eval()
+        predictions, labels = [], []
+        eval_bar = tqdm(
+            range(len(self.dataloader_eval)),
+            disable=not self.accelerator.is_local_main_process,
+        )
+        for step, batch in enumerate(self.dataloader_eval):
+            outputs = self.model(**batch)
+            predictions.extend(
+                torch.sigmoid(self.accelerator.gather(outputs.logits))
+                .detach()
+                .view(-1)
+                .tolist()
+            )
+            labels.extend(batch["labels"].tolist())
+            eval_bar.update(1)
+        self.model.train()
+        eval_metric = self.metric_fn(np.array(predictions), labels)
+        return eval_metric
+
+    def train(self):
+        total_batch_size = self.batch_size_train * self.accelerator.num_processes
+
+        num_samples = len(self.dataloader_train.dataset)
+        logger.info("***** Running training *****")
+        logger.info(f"  Num samples = {num_samples}")
+        logger.info(f"  Batch size per device = {self.batch_size_train}")
+        logger.info(
+            f"  Total train batch size (w. parallel, distributed & accumulation) "
+            f"= {total_batch_size}"
+        )
+        logger.info(
+            f"  Gradient Accumulation steps = {self.gradient_accumulation_steps}"
+        )
+        logger.info(f"  Total optimization steps = {self.max_steps}")
+        logger.info(
+            f"  Total epochs approx. = "
+            f"{(self.max_steps*total_batch_size)/num_samples:.2f}"
+        )
+        progress_bar = tqdm(
+            range(self.max_steps), disable=not self.accelerator.is_local_main_process
+        )
+        losses = deque(maxlen=self.log_steps // 2)
+        continue_training, completed_steps = True, 0
+        while continue_training:
+            self.model.train()
+            for step, batch in enumerate(self.dataloader_train):
+                outputs = self.model(**batch)
+                loss = outputs.loss
+                self.accelerator.backward(loss)
+                self.optimizer.step()
+                self.scheduler.step()
+                self.optimizer.zero_grad()
+                progress_bar.update(1)
+                completed_steps += 1
+
+                losses.append(loss.item())
+
+                if completed_steps >= self.max_steps:
+                    continue_training = False
+                    break
+
+                if completed_steps % self.log_steps == 0:
+                    logger.info(
+                        f"Step {completed_steps} | "
+                        f"train loss {sum(losses)/len(losses):.5f} | "
+                        f"lr {self.scheduler.get_last_lr()[0]:.3E}"
+                    )
+
+                eval_metric = None
+                if (completed_steps > 0) and (completed_steps % self.eval_steps == 0):
+                    logger.info("Evaluating.")
+                    eval_metric = self.evaluate(self.dataloader_eval)
+                    logger.info(f"Step {completed_steps}: {eval_metric}")
+
+                if completed_steps % self.save_steps == 0:
+                    self.accelerator.wait_for_everyone()
+                    unwrapped_model = self.accelerator.unwrap_model(self.model)
+                    chkpt_dir = os.path.join(
+                        self.model_dir, f"checkpoint-{completed_steps}"
+                    )
+                    unwrapped_model.save_pretrained(
+                        chkpt_dir, save_function=self.accelerator.save
+                    )
+                    self.accelerator.save(
+                        self.optimizer, os.path.join(chkpt_dir, "optimizer.pt")
+                    )
+                    self.accelerator.save(
+                        self.tokenizer, os.path.join(chkpt_dir, "tokenizer.pt")
+                    )
+                    # If we did not evaluate at this step, we must get the
+                    # evaluate to get the metric for comparison to earlier checkpoints
+                    if eval_metric is None:
+                        eval_metric = self.evaluate(self.dataloader_eval)
+
+                    if (self.best_model_score is None) or (
+                        eval_metric[self.metric_name] > self.best_model_score
+                    ):
+                        self.best_model_score = eval_metric[self.metric_name]
+                        self.best_model_checkpoint = chkpt_dir
+
+                    # FIXME: USE MLFLOW ARTIFACT
+                    # if (self.mlflow_tracking_uri is not None) and (
+                    #     self.mlflow_save_models == "all"
+                    # ):
+                    #     # FIXME: Save model to mlflow artifact store
+                    #     pass
